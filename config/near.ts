7aa8bd03257630d34a671b243e57c5b0e99155ec
ducle <ducle@chidoanh.com>
2022-02-27 18:49:53 +0700

    update contract id

--- config/near.ts
+++ config/near.ts
@@ -7,7 +7,7 @@ export const NearConfig = Object.freeze({
         'https://rpc.testnet.internal.near.org',
     contractName:
         process.env.REACT_APP_NEAR_CONTRACT_NAME ||
-        'dev-1645939332663-15822937666472',
+        'dev-1645962153518-40510791549962',
     walletUrl:
         process.env.REACT_APP_NEAR_WALLET_URL ||
         'https://wallet.testnet.near.org',
