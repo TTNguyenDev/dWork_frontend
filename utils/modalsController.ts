3ea41ccd80a1b5c169a37e88f8a921b33cc273fc
ducle <ducle@chidoanh.com>
2022-05-02 17:23:48 +0700

    deposit info create account

--- utils/modalsController.ts
+++ utils/modalsController.ts
@@ -1,19 +1,26 @@
-
 export type ModalsControllerType = {
     openConnectWalletModal: () => void;
     openCreateTaskModal: () => void;
     closeCreateTaskModal: () => void;
-}
+    openDepositInfomationModal: () => void;
+    closeDepositInfomationModal: () => void;
+    setDataDepositInfomationModal: (payload: {
+        amount: number;
+        reason: string;
+        action: any;
+    }) => any;
+};
 
 export class ModalsController {
-
     private static _controller: ModalsControllerType = {} as any;
 
     static get controller() {
         return this._controller;
     }
 
-    static setController(controller: Partial<Record<keyof ModalsControllerType, () => void>>) {
-        this._controller = { ...this._controller, ...controller }
+    static setController(
+        controller: Partial<Record<keyof ModalsControllerType, () => void>>
+    ) {
+        this._controller = { ...this._controller, ...controller };
     }
 }
